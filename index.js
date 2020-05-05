e340bcb280f2a3e16a60072a9657e04aed30e768
BuildTools <unconfigured@null.spigotmc.org>
2020-05-05 18:16:52 +0200

    fixed port

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.delete('/api/persons/:id',(req,res)=>{
 app.get('/info',(req,res)=>{
     res.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${Date()}</p>`)
 })
-const PORT = 3001
+const PORT =  process.env.PORT||3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
