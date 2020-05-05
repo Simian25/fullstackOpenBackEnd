eacd640600b9bf7c0a8ac9eb1ee48526a798ac87
BuildTools <unconfigured@null.spigotmc.org>
2020-05-05 15:56:58 +0200

    finished exercises 3.1-3.6

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const app = express()
+app.use(express.json());
+let persons = [
+    { 
+      "name": "Arto Hellas", 
+      "number": "040-123456",
+      "id": 1
+    },
+    { 
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    { 
+      "name": "Dan Abramov", 
+      "number": "12-43-234345",
+      "id": 3
+    },
+    { 
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122",
+      "id": 4
+    }
+  ]
+
+  const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+
+
+
+  app.get('/',(req,res) => {
+    res.send('<h1>wrong entry </h1>')
+  })
+app.get('/api/persons',(req,res)=>{
+    res.json(persons)
+})
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name||!body.number) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+  if(persons.find(person => person.name===body.name)){
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    }) 
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+
+
+
+app.get('/api/persons/:id',(req,res)=>{
+    const id = Number(req.params.id)
+    const person = persons.find((pers) => pers.id===id)
+    if(person){
+      res.json(person);
+    }else{
+      res.status(404).end()
+    }
+    
+})
+app.delete('/api/persons/:id',(req,res)=>{
+  const id = Number(req.params.id);
+  persons = persons.filter((pers)=>pers.id!==id)
+  res.status(204).end();
+})
+
+app.get('/info',(req,res)=>{
+    res.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${Date()}</p>`)
+})
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
